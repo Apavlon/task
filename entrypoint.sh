19e477a261bb333a20076219c3b0d2661a7e09b2
PavelArtemenko <brame1brame@gmail.com>
2025-04-29 14:31:59 +0600

    entrypoint.sh is fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+echo "Creating staticfiles directory..."
 mkdir -p /app/staticfiles
+echo "Setting permissions for staticfiles..."
+chmod -R 755 /app/staticfiles
+echo "Running migrations..."
+python manage.py migrate
+echo "Running collectstatic..."
+python manage.py collectstatic --noinput --verbosity 2 --traceback
+echo "Starting Daphne..."
+daphne -b 0.0.0.0 -p 8000 store.asgi:application
+
+#!/bin/bash
+echo "Creating staticfiles directory..."
+mkdir -p /app/staticfiles
+echo "Setting permissions for staticfiles..."
 chmod -R 755 /app/staticfiles
 echo "Running migrations..."
 python manage.py migrate
